11f76d36afea4d2af7a62882640d0d395d92de1f
yenmangu <robert.shelford@googlemail.com>
2022-08-31 22:10:22 +0100

    new structure again

--- html-root/server.js
+++ html-root/server.js
@@ -9,9 +9,55 @@ app.use('/css', express.static('css'));
 app.use('/images', express.static('images'));
 app.use('/js', express.static('js'));
 
+console.log(process.cwd());
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 	console.log(req.url.pathname);
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(__dirname + '/contact.html');
 });
 
 app.listen(8080);
